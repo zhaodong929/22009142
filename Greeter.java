c315e495fd3140cc2f3591c90773b34733bb3614
zhaodong <425016193@qq.com>
2023-09-03 10:08:01 +0800

    Fixed bug

--- Greeter.java
+++ Greeter.java
@@ -17,6 +17,10 @@ public class Greeter {
   }
 
   public static void main(String[] args) {
+    if (args.length < 3) {
+      System.out.println("Usage: java Greeter <language> <country> <name>");
+      return;
+    }
     String language = args[0];
     String country = args[1];
     String name = args[2];
